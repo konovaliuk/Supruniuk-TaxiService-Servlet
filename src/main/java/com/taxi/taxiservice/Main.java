05e1990b9bc9b49b62cc897d20a375c5ce395d40
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-04-23 22:34:04 +0300

    added project setup

---
+++ src/main/java/com/taxi/taxiservice/Main.java
@@ -0,0 +1,27 @@
+package com.taxi.taxiservice;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "servlet", value = "/servlet")
+public class Main extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
